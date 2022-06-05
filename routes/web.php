48a827c2d81b5ca118cbacd9ee20618f204eac87
wenderson guedes <wendersonguedes6@gmail.com>
2022-06-05 15:37:53 -0300

    Route file

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Livewire\Despesas;
 use App\Http\Livewire\Home;
 use App\Http\Livewire\Login;
+use App\Http\Livewire\Objetivos;
 use App\Http\Livewire\Receitas;
 use App\Http\Livewire\Register;
 use Illuminate\Support\Facades\Hash;
@@ -10,9 +11,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', Home::class)->name('home');
 
+Route::get('/receitas', Receitas::class)->name('receitas');
+
 Route::get('/despesas', Despesas::class)->name('despesas');
 
-Route::get('/receitas', Receitas::class)->name('receitas');
+Route::get('/objetivos', Objetivos::class)->name('objetivos');
 
 Route::get('/login', Login::class)->name('login');
 
